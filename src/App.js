93d01fdca107754a02f777239b521576b5103e00
Amarprit <amarpritkaur.kharbanda@citrix.com>
2024-02-16 13:26:37 +0530

    learning state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let x = 10;
+
+  function handleUpdate() {
+    x = x + 5;
+  }
+  console.log("x: " + x);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Learning state {x}</h1>
+      <button onClick={handleUpdate}>Update </button>
+    </>
   );
 }
 
